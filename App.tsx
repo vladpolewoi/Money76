27409f5e5315ae81013f4ca2cf2b5287585a79c9
vlad polevoi <vlad.polewoi1@gmail.com>
2021-01-26 20:22:21 +0200

    start

--- App.tsx
+++ App.tsx
@@ -1,23 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import { Text, View } from 'react-native';
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
-
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
+  return (
+    <View style={{
+      alignItems: "center",
+      justifyContent: "center",
+      flex: 1
+    }}>
+      <Text>MONEY 76</Text>
+    </View>
+  );
 }
